014657f15dcf5dd7874e60694acec447a221310f
Sabrine <Sabrine.RIAHI@student.umons.ac.be>
2017-11-03 10:09:34 +0100

    Exercice 1

--- src/main/java/exercise1/ExceptionThrower.java
+++ src/main/java/exercise1/ExceptionThrower.java
@@ -1,7 +1,11 @@
 package exercise1;
 
 public class ExceptionThrower {
-	void throwACustomExceptionWhenValueIs42(int value) {
 
+	public void throwACustomExceptionWhenValueIs42(int value) {
+
+		if (value==42){
+			throw new NotreException();
+		}
 	}
 }
